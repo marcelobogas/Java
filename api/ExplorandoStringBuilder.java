36f9c62ef13ad4f26a005a00e70cfefff27c2120
Marcelo Cerezo Bogás <marcelo.bogas@fatec.sp.gov.br>
2018-08-26 18:33:18 -0300

    Pasta API

---
+++ api/ExplorandoStringBuilder.java
@@ -0,0 +1,17 @@
+package api;
+
+public class ExplorandoStringBuilder {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("João");
+		sb.append(", ").append("Maria");
+		sb.append(", ").append("Pedro");
+		
+		//é mutável
+		//sb.reverse();
+		System.out.println(sb.toString());
+		System.out.println(sb.substring(1, 3));
+		
+	}
+}
